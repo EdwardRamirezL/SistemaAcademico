4f51cc932bd35a27415295421a65ab2679f2d8a0
Eduard Ramirez <ramirezedward535@gmail.com>
2026-03-01 10:37:59 -0500

    feat: crear estructura base de Main con ArrayLists

--- src/sistemaacademico/SistemaAcademico.java
+++ src/sistemaacademico/SistemaAcademico.java
@@ -1,20 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package sistemaacademico;
 
-/**
- *
- * @author Eduard Ramirez
- */
+import java.util.ArrayList;
+
 public class SistemaAcademico {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        ArrayList<Object> estudiantes = new ArrayList<>();
+        ArrayList<Object> asignaturas = new ArrayList<>();
+        ArrayList<Object> notas = new ArrayList<>();
     }
     
 }
