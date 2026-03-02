3a3adec0bc0e9714d286fb26a1f7a1a51386fe57
Dayanna Mendoza <dayannammendoza@unicesar.edu.co>
2026-03-02 08:23:03 -0500

    feat: agregar encapsulamiento a clase Estudiante

--- src/sistemaacademico/Estudiante.java
+++ src/sistemaacademico/Estudiante.java
@@ -15,4 +15,46 @@ public class Estudiante {
     private String programa;
     private int semestre;
     
+    // El constructor xd
+    public Estudiante(String codigo, String nombre, String programa, int semestre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.programa = programa;
+        this.semestre = semestre;
+    }
+
+    // Getters (☞ﾟヮﾟ)☞
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPrograma() {
+        return programa;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    // Setters ☜(ﾟヮﾟ☜)
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrograma(String programa) {
+        this.programa = programa;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+    
 }
