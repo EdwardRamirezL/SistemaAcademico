881fe6607edc81098023b6ce10dc170a4397543a
Dayanna Mendoza <dayannammendoza@unicesar.edu.co>
2026-03-02 08:19:38 -0500

    feat: crear clase Estudiante con atributos

---
+++ src/sistemaacademico/Estudiante.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademico;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Estudiante {
+    
+    private String codigo;
+    private String nombre;
+    private String programa;
+    private int semestre;
+    
+}
